fa9d8962c3df6aa541e6f131f6a9e72df835d4f0
GomezValentin <gomezvalentin.dev@gmail.com>
2024-01-25 21:47:52 -0300

    Trabaj exception con funciones.

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,4 @@
-var_1 = input("Ingrese primer numero: ")
-var_2 = input("Ingrese el segundo numero: ")
+########## FUNCIONES Y EXCEPT ###############
 
 def prueba(var_1,var_2):
     try:
@@ -8,6 +7,23 @@ def prueba(var_1,var_2):
     except TypeError as Error:
         print(f"Has intentado {Error}")
         print(f"Igualmente lo resuelvo y el resultado es : {int(var_1)+int(var_2)} ")
-        
+        eleccion = input("Quiere agregar otro numero ? :")
+        if eleccion == "Si" or eleccion== "si":
+            numero1 = int(input("Agregue el primer numero :"))
+            numero2 = int(input("Ingrese el segundo numero: "))
+            prueba(numero1,numero2)
+    #Si no hay error va a entrar al else        
+    else:
+         eleccion = input("Quiere agregar otro numero ? :")
+         if eleccion == "Si" or eleccion== "si":
+            numero1 = int(input("Agregue el primer numero :"))
+            numero2 = int(input("Ingrese el segundo numero: "))
+            prueba(numero1,numero2) 
+    #Si pone que no en ambos condicionales, el programa finaliza con un saludo
     finally:
         print("Gracias por usar nuestra App")
+
+################
+var_1 = int(input("Ingrese primer numero: "))
+var_2 = int(input("Ingrese el segundo numero: "))
+prueba(var_1,var_2)
