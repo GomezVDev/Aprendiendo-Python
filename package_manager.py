e3db69ca457725301bec167d16e435e61b10dace
GomezValentin <gomezvalentin.dev@gmail.com>
2024-02-07 16:05:06 -0300

    Manejo de paquetes y prueba modulos.

---
+++ package_manager.py
@@ -0,0 +1,13 @@
+import numpy
+
+numpy_array = numpy.array([1,2,3,4,5,6,7,8,9])
+print(type(numpy_array)) 
+print(numpy_array * 2) #Nueva operacion
+
+#import pandas
+
+
+import prueba_modulos
+
+resultado = prueba_modulos.suma(1,2)
+print(resultado)
---
+++ prueba_modulos.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    print("Esto se printea si lo tengo en el if init")
+
+def suma(num1,num2):
+    return num1+num2
+    
+#print("Esto se printea si no tengo el if init") En efecto
+
